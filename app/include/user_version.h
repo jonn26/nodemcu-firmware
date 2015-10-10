8e8ef5badbc3ed3d6bcd0dff83188a38808b0506
jonn26 <jonn26@gmail.com>
2015-10-10 12:55:12 -0700

    Travis, do it

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -6,7 +6,7 @@
 #define NODE_VERSION_REVISION	6U
 #define NODE_VERSION_INTERNAL   0U
 
-#define NODE_VERSION	"NodeMCU 0.9.6 - Jon2"
-#define BUILD_DATE	    "build 20151005"
+#define NODE_VERSION	"NodeMCU 0.9.5 - u8g"
+#define BUILD_DATE	    "build 20151010"
 
 #endif	/* __USER_VERSION_H__ */
